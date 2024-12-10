4308efdf589d854016f9ac9f37db827a77a3ff53
finder1793 <kangas.kyle@gmail.com>
2024-12-10 19:44:49 +0000

    mechanic Builder start

---
+++ src/main/java/me/asleepp/skriptnexo/elements/effects/EffRegisterMechanic.java
@@ -0,0 +1,33 @@
+package me.asleepp.skriptnexo.elements.effects;
+
+import ch.njol.skript.Skript;
+import ch.njol.skript.doc.Description;
+import ch.njol.skript.doc.Examples;
+import ch.njol.skript.doc.Name;
+import ch.njol.skript.lang.Effect;
+import ch.njol.skript.lang.Expression;
+import ch.njol.skript.lang.SkriptParser;
+import ch.njol.util.Kleenean;
+import org.bukkit.event.Event;
+
+@Name("Register New Nexo Mechanic")
+@Description({"Register New Nexo Mechanic to use at your leasure"})
+@Examples({"create mechanic"})
+@Since("1.0.5")
+@RequiredPlugins("Nexo")
+
+public class EffRegisterMechanic extends Effect {
+
+    static  {
+        Skript.registerEffect(EffRegisterMechanic.class, "create mechanic %string% with: %objects%");
+    }
+    @Override
+    protected void execute(Event event) {
+        //pass the mechanic name and config to Nexo
+        new MechanicLoader().loadMechanic(mechanic Name, mechanicConfig);
+    }
+    @Override
+    public String toString(Event event, boolean debug) {
+        return "Register mechanic: " + mechanicName;
+    }
+}
---
+++ src/main/java/me/asleepp/skriptnexo/elements/effects/mechanicbuilder/MechanicLoader.java
@@ -0,0 +1,13 @@
+package me.asleepp.skriptnexo.elements.effects.mechanicbuilder;
+
+
+
+public class MechanicLoader {
+    public void loadMechanic(String mechanicName, Map<String, Object> config) {
+        // Converts the Mechanic to a format Nexo understands
+        Mechanic newMechanic = new Mechanic(mechanicName, config);
+
+        // Use Nexo Registry to add Mechanic
+        NexoMechanicRegistry(newMechanic);
+    }
+}
