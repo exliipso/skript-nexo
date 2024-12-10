6b7771e13065fd0d5ac8277c74826e96c691f4a1
finder1793 <kangas.kyle@gmail.com>
2024-12-10 23:23:50 +0000

    Maybe

--- src/main/java/me/asleepp/skriptnexo/elements/effects/mechanicbuilder/MechanicLoader.java
+++ src/main/java/me/asleepp/skriptnexo/elements/effects/mechanicbuilder/MechanicLoader.java
@@ -1,16 +1,16 @@
 package me.asleepp.skriptnexo.elements.effects.mechanicbuilder;
 
 import java.util.Map;
-import com.nexomc.nexo.mechanics.Mechanic;
+import com.nexomc.nexo.mechanics.MechanicManager;
 import com.nexomc.nexo.mechanics.MechanicFactory;
+import com.nexomc.nexo.mechanics.Mechanic;
 
-
-public abstract class MechanicLoader {
+public class MechanicLoader {
     public void loadMechanic(String mechanicName, Map<String, Object> config) {
         // Converts the Mechanic to a format Nexo understands
         Mechanic newMechanic = new Mechanic(mechanicName, config);
 
         // Use Nexo Registry to add Mechanic
-        MechanicsManager(newMechanic);
+        NexoMechanicRegistry(newMechanic);
     }
 }
