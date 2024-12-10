448842792ddf77843349dea158da5b98439801c8
finder1793 <kangas.kyle@gmail.com>
2024-12-10 22:36:49 +0000

    ahhh

--- src/main/java/me/asleepp/skriptnexo/elements/effects/mechanicbuilder/MechanicLoader.java
+++ src/main/java/me/asleepp/skriptnexo/elements/effects/mechanicbuilder/MechanicLoader.java
@@ -1,6 +1,9 @@
 package me.asleepp.skriptnexo.elements.effects.mechanicbuilder;
 
 import java.util.Map;
+import com.nexomc.nexo.mechanics.Mechanic;
+import com.nexomc.nexo.mechanics.MechanicFactory;
+
 
 public class MechanicLoader {
     public void loadMechanic(String mechanicName, Map<String, Object> config) {
